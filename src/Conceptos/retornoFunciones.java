be36f2b109c9b8c4ca1ede74cd3fb6eb78fa143f
JackDelAguila <jackdelaguilavilla03@gmail.com>
2022-09-11 19:46:08 -0500

    new commit

---
+++ src/Conceptos/retornoFunciones.java
@@ -0,0 +1,11 @@
+package Conceptos;
+
+public class retornoFunciones {
+    public static void main(String[] args) {
+
+    }
+
+    private static void Holamundo(){
+        System.out.println("Hola mundo privado");
+    }
+}

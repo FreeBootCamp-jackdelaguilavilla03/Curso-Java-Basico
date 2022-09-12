1815dddfc0c86f1d8064d78873f39931468ddfaa
JackDelAguila <jackdelaguilavilla03@gmail.com>
2022-09-11 20:03:04 -0500

    new commit

---
+++ src/Conceptos/Funciones.java
@@ -0,0 +1,19 @@
+package Conceptos;
+
+public class Funciones {
+    public static void holamundo(){
+        System.out.println("Hola mundo");
+    }
+    protected static void adiosmundo(){
+        System.out.println("Adios mundo");
+    }
+
+    public static int suma(int a, int b){
+        return a+b;
+    }
+
+    public static float suma(float a, float b){
+        return a+b;
+    }
+
+}
--- src/Conceptos/retornoFunciones.java
+++ src/Conceptos/retornoFunciones.java
@@ -2,10 +2,16 @@ package Conceptos;
 
 public class retornoFunciones {
     public static void main(String[] args) {
+        //el public permite tener acceso al metodo de cualquier parte del programa
+        Funciones.holamundo();
+        //el metodo protected te permite acceder a los metodos siempre y cuando sea del mismo package
+        Funciones.adiosmundo();
 
-    }
-
-    private static void Holamundo(){
-        System.out.println("Hola mundo privado");
+        //Sobre carga de funciones es funcional dependiendo el tipo de variable que se ingresa por parametro
+        int sumatotal = Funciones.suma(1, 2);
+        float sumatotal2 = Funciones.suma(1.5f, 2.5f);
+        System.out.println("1 + 2  = " + sumatotal);
+        System.out.println("1.5 + 2.5 = " + sumatotal2);
     }
 }
+

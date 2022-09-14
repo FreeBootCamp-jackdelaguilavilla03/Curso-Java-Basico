ac25c38a2d3129f03e5cfc7d6952e1d233f34583
JackDelAguila <jackdelaguilavilla03@gmail.com>
2022-09-14 13:28:48 -0500

    commit

---
+++ src/control/control.java
@@ -0,0 +1,13 @@
+package control;
+
+public public class control {
+  public static void main(String[] args) {
+    String concat;
+    String[] nombres = new String{"Alan","Garcia"}
+    for (String nombre : nombres) {
+       concat += nombre;
+      
+    }
+    System.out.println(contact);
+  }
+}

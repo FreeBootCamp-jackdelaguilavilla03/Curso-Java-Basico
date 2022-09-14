6ce0bbbf92006721723266ba3997765e734587aa
JackDelAguila <jackdelaguilavilla03@gmail.com>
2022-09-14 12:50:18 -0500

    feat(ejercicio):new exercise

---
+++ src/tema1-ejercicio1/ejercicio1.java
@@ -0,0 +1,12 @@
+class ejercicio {
+  public static void main(String[] args) {
+    int numero = 1;
+    float flotante = 1.3f;
+    String palabra = "Holas";
+
+    System.out.println(numero);
+    System.out.println(flotante);
+    System.out.println(palabra);
+
+  }
+}

cadff8f0c87ba5687fe70ecdd7d6137757447a9d
JackDelAguila <jackdelaguilavilla03@gmail.com>
2022-09-14 13:22:10 -0500

    commit

--- src/funciones/funciones.java
+++ src/funciones/funciones.java
@@ -1,11 +1,13 @@
-class funciones {
+package funciones;
+
+public class funciones {
   public static void main(String[] args) {
     int precio = 19;
     System.out.println(ivaPrecio(precio));
-    
   }
-  public int  ivaPrecio (int precio){
+
+  public static int ivaPrecio (int precio){
     // NO se que es un IVA XDDDDD lo pondre con IGV como en mi pais :3
-    return precio += precio*18;
+    return precio + precio*0.18;
   }
 }
